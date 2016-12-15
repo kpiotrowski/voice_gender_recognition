e680dec9d59fd76a4fdd9dbef84b58b4a7aebd87
Kamil <kpiotrowski@comea.pl>
2016-12-16 00:44:10 +0100

    more samples

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import glob
 from scipy.io import wavfile
 
-#male: 1 female: 0
-shouldBeMale = 10
-shouldBeFemale = 10
-foundMale = 8
-foundFemale = 12
 
-files = glob.glob("samples/*.wav")
-for file in files:
+def HPS(array):
 
 
-
-    array = wavfile.read(file)
-    shouldBe = int(file.replace("/","_").replace(".","_").split("_")[2] == "M")
+    return 1
 
 
-    print(str(shouldBe) + " : " + file + " " + str(array))
+if __name__ == "__main__":
+    # male: 1 female: 0
+    shouldBe = [0,0]
+    found = [0,0]
+    files = glob.glob("samples/*.wav")
+    for file in files:
+        array = wavfile.read(file)
+        shouldBe[int(file.replace("/", "_").replace(".", "_").split("_")[2] == "M")]+=1
 
 
-print("MACIERZ POKRYCIA:")
-M = [ [min(shouldBeMale, foundMale), max(0,shouldBeMale-foundMale)], [max(0,shouldBeFemale-foundFemale), min(shouldBeFemale, foundFemale)] ]
-wsp = (M[0][0]+M[1][1])/(sum(M[0])+sum(M[1]))
-print(wsp)
+        print(str(shouldBe) + " : " + file + " " + str(array))
+
+    print("MACIERZ POKRYCIA:")
+    M = [[min(shouldBe[1], found[1]), max(0, shouldBe[1] - found[1])],
+         [max(0, shouldBe[0] - found[0]), min(shouldBe[0], found[0])]]
+    print(M)
+    wsp = (M[0][0] + M[1][1]) / (sum(M[0]) + sum(M[1]))
+    print(wsp)
