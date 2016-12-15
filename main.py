e0cd4c3587662b411236f4374d8020000e7da952
Kamil <kpiotrowski@comea.pl>
2016-12-16 00:26:16 +0100

    coverage matrix

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import glob
 from scipy.io import wavfile
 
+#male: 1 female: 0
+shouldBeMale = 10
+shouldBeFemale = 10
+foundMale = 8
+foundFemale = 12
+
 files = glob.glob("samples/*.wav")
-i = 0
 for file in files:
-    i += 1
-    print(file)
+
+
+
     array = wavfile.read(file)
+    shouldBe = int(file.replace("/","_").replace(".","_").split("_")[2] == "M")
 
-    print(str(i) + " : " + file + " " + str(array))
+
+    print(str(shouldBe) + " : " + file + " " + str(array))
 
 
-print("KONIEC")
+print("MACIERZ POKRYCIA:")
+M = [ [min(shouldBeMale, foundMale), max(0,shouldBeMale-foundMale)], [max(0,shouldBeFemale-foundFemale), min(shouldBeFemale, foundFemale)] ]
+wsp = (M[0][0]+M[1][1])/(sum(M[0])+sum(M[1]))
+print(wsp)
