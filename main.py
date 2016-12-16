74a3575d8cd2d8c7daad4ffeea94c23b3b92ec46
Kamil <kpiotrowski@comea.pl>
2016-12-17 00:37:07 +0100

    autokorelacja - najlepsze parametry

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from scipy.io import wavfile
 from pylab import *
 from scipy import *
 
-maleFemaleFreq = [120, 220]
+maleFemaleFreq = [120, 232]
 TS=3 #time for simple method
 
 humanVoiceMinMAx = [80,255]
@@ -40,9 +40,9 @@ def listVariation(list1, list2): return sum([ abs(int(x)-int(y)) for x,y in zip(
 
 if __name__ == "__main__":
     #wspMax = 0
-    #for MM in range(60,180,4):
-        #for K in range(160,280,4):
-            #maleFemaleFreq=[MM,K]
+    # for MM in range(119,122,1):
+    #     for K in range(232,235,1):
+    #         maleFemaleFreq=[MM,K]
 
     # male: 1 female: 0
     M = [[0,0],[0,0]]
@@ -55,11 +55,11 @@ if __name__ == "__main__":
         #print(found)
         M[shouldBe][found]+=1
     #print("MACIERZ POKRYCIA:")
-    print(M)
+    #print(M)
     wsp = (M[0][0] + M[1][1]) / (sum(M[0]) + sum(M[1]))
     print(wsp)
 
-
-            #if(wsp-wspMax>0.0001):
+            #
+            # if(wsp-wspMax>0.0001):
             #    wspMax = wsp
             #    print("WSP: "+str(wsp)+" M: "+str(MM)+" K: "+str(K))
