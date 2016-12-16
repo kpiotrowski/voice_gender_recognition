69a4950df2a03b5b446776c433d8dafd6ad0ea86
Kamil <kpiotrowski@comea.pl>
2016-12-16 03:14:06 +0100

    metoda autokorelacji

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pylab import *
 from numpy import *
 from scipy import *
 
+maleFemaleFreq = [132, 210]
 
 def HPS(rate, data):
     T = 1
@@ -21,27 +22,38 @@ def HPS(rate, data):
     return 1
 
 def simpleRecognition(rate, data):
-    maleFemaleFreq = [132, 210]
     if(checkBaseFreq(maleFemaleFreq[0], rate, data) < checkBaseFreq(maleFemaleFreq[1], rate, data)): return 1
     return 0
 def checkBaseFreq(freq, ratio, data):
+    T=1
     box = int(1/freq*ratio)
-    return average([listVariation(data[i*box:(i+1)*box-1],data[(i+1)*box:(i+2)*box-1]) for i in range( max(int(len(data)/box)-1, 10)) ])
-
-def listVariation(list1, list2): return sum([ ((x-y)^2)/10000 for x,y in zip(list1, list2)])
+    return average([listVariation(data[i*box:(i+1)*box-1],data[(i+1)*box:(i+2)*box-1]) for i in range(
+        int(len(data)/box/2-(T/2*freq*box/2)),
+        int(len(data)/box/2+(T/2*freq*box/2)),1)])
+def listVariation(list1, list2): return sum([ abs(int(x)-int(y)) for x,y in zip(list1, list2)])
 
 if __name__ == "__main__":
-    # male: 1 female: 0
-    M = [[0,0],[0,0]]
+    for M in range(80,180,10):
+        for K in range(160,260,10):
+            maleFemaleFreq = [M,K]
+            wspMax = 0
 
-    files = glob.glob("samples/*.wav")
-    for file in files:
-        rate, array = wavfile.read(file)
-        shouldBe = int(file.replace("/", "_").replace(".", "_").split("_")[2] == "M")
-        #found = HPS(rate, array)
-        found = simpleRecognition(rate, array)
-        M[shouldBe][found]+=1
-    print("MACIERZ POKRYCIA:")
-    print(M)
-    wsp = (M[0][0] + M[1][1]) / (sum(M[0]) + sum(M[1]))
-    print(wsp)
+            # male: 1 female: 0
+            M = [[0,0],[0,0]]
+            files = glob.glob("samples/*.wav")
+            for file in files:
+                rate, array = wavfile.read(file)
+                shouldBe = int(file.replace("/", "_").replace(".", "_").split("_")[2] == "M")
+                #found = HPS(rate, array)
+                found = simpleRecognition(rate, array)
+                print(found)
+                M[shouldBe][found]+=1
+            print("MACIERZ POKRYCIA:")
+            print(M)
+            wsp = (M[0][0] + M[1][1]) / (sum(M[0]) + sum(M[1]))
+            print(wsp)
+
+            
+            if(wspMax<wsp):
+                wspMax = wsp
+                print("M: "+str(M)+" K: "+str(K))
