5cedfdbcac63b122539fc525d9d08f6199d10aaf
Gabriel Ruiz <94541194+gabiru05@users.noreply.github.com>
2023-11-14 03:07:54 -0500

    Pruebas con Microfono
    °Realización de pruebas
    +Adaptaciones con audio proveniente del microfono
    +Documentacion

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-
-
 # Gender Voice Recognition in Python
 
 Este proyecto es un fork del repositorio original, que ha estado inactivo durante varios años. Mi objetivo es continuar el desarrollo y mejorar la funcionalidad del reconocimiento de género por voz.
--- main.py
+++ main.py
@@ -1,66 +1,79 @@
-import glob
-from scipy.io import wavfile
-from pylab import *
-from scipy import *
-from numpy.fft import fft
+import sounddevice as sd
+import numpy as np
+from scipy.fftpack import fft
+from copy import copy
 
-maleFemaleFreq = [120, 232]
-TS=3 #time for simple method
+# Define la duración de la grabación en segundos
+DURATION = 5
 
-humanVoiceMinMAx = [80,255]
-maleMinMax=[60,160]
-femaleMinMax=[180,270]
-HPSLoop=5
+# Define la frecuencia de muestreo (en Hz)
+SAMPLE_RATE = 44100
 
-def HPS(rate, dataVoice):
+# Define los rangos de frecuencia para voces masculinas y femeninas
+maleMinMax = (75, 150)
+femaleMinMax = (150, 255)
+
+# Define el número de iteraciones para el método HPS
+HPSLoop = 5
+
+# Define la función HPS
+def HPS(rate, data):
+    dataVoice = data
     T = 3  # time for HPS method
 
-    if( T >len(dataVoice)/rate): T = len(dataVoice)/rate
+    if T > len(dataVoice) / rate:
+        T = len(dataVoice) / rate
     dataVoice = dataVoice[max(0, int(len(dataVoice) / 2) - int(T / 2 * rate)):min(len(dataVoice) - 1, int(len(dataVoice) / 2) + int(T / 2 * rate))]
-    partLen=int(rate)
-    parts = [ dataVoice[i*partLen:(i+1)*partLen] for i in range(int(T))]
+    partLen = int(rate)
+    parts = [dataVoice[i * partLen:(i + 1) * partLen] for i in range(int(T))]
     resultParts = []
+    
+    fftR = np.zeros(len(dataVoice))  # Inicializa fftR aquí
+
     for data in parts:
-        if(len(data)==0): continue
         window = np.hamming(len(data))
-        data = data*window
-        fftV = abs(fft(data))/rate
+        data = data * window
+        fftV = abs(fft(data)) / rate
         fftR = copy(fftV)
-        for i in range(2,HPSLoop):
+        for i in range(2, HPSLoop):
             tab = copy(fftV[::i])
             fftR = fftR[:len(tab)]
             fftR *= tab
         resultParts.append(fftR)
-    result = [0]*len(resultParts[int(len(resultParts)/2)])
+
+    # Asegúrate de que resultParts tenga al menos un elemento
+    if not resultParts:
+        resultParts.append(np.zeros(len(fftR)))
+
+    result = np.zeros(len(resultParts[0]))
+
     for res in resultParts:
-        if(len(res)!=len(result)): continue
-        result += res
+        if len(res) != len(result):
+            continue
+        result = np.add(result, res)
 
-    if(sum(result[maleMinMax[0]:maleMinMax[1]]) > sum(result[femaleMinMax[0]:femaleMinMax[1]])): return 1
-    return 0
+    if sum(result[maleMinMax[0]:maleMinMax[1]]) > sum(result[femaleMinMax[0]:femaleMinMax[1]]):
+        return "Male"
+    return "Female"
 
-def simpleRecognition(rate, data):
-    if(checkBaseFreq(maleFemaleFreq[0], rate, data) < checkBaseFreq(maleFemaleFreq[1], rate, data)): return 1
-    return 0
-def checkBaseFreq(freq, ratio, data):
-    box = int(1/freq*ratio)
-    return sum([listVariation(data[int(i*box):int((i+1)*box-1)],data[int((i+1)*box):int((i+2)*box-1)]) for i in range( max(0,int(len(data)/box/2-(TS/2*freq))), min(int(len(data)/box)-2,int(len(data)/box/2+(TS/2*freq))),1)])
-def listVariation(list1, list2): return sum([ abs(int(x)-int(y)) for x,y in zip(list1, list2)])
+# Define la función de callback que se llamará para cada bloque de audio
+def callback(indata, frames, time, status):
+    if status:
+        print(f"Error en la captura de audio: {status}")
+        return
 
-if __name__ == "__main__":
-    x=1
-    # male: 1 female: 0
-    M = [[0,0],[0,0]]
-    files = glob.glob("samples/*.wav")
-    for file in files:
-        rate, array = wavfile.read(file)
-        shouldBe = int(file.replace("/", "_").replace(".", "_").split("_")[2] == "M")
-        found = HPS(rate, array)
-        #found = simpleRecognition(rate, array)
-        M[shouldBe][found]+=1
-     
-        print(x)
-        x=x+1
-    print(M)
-    wsp = (M[0][0] + M[1][1]) / (sum(M[0]) + sum(M[1]))
-    print(wsp)
+    # Convierte los datos de audio a un array de NumPy
+    data = np.frombuffer(indata, dtype=np.float32)
+
+    # Aplica la función HPS a los datos de audio
+    gender = HPS(SAMPLE_RATE, data)
+
+    # Aquí puedes hacer algo con el resultado (por ejemplo, imprimirlo)
+    print(gender)
+
+# Crea un objeto de entrada de audio con la función de callback
+stream = sd.InputStream(callback=callback, channels=1, samplerate=SAMPLE_RATE)
+
+# Inicia la captura de audio
+with stream:
+    sd.sleep(int(DURATION * 1000))
